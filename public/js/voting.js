3b8d2c61933d10e0d3e6e7a596c6046ca9a2b050
kospaolo <kos.paolo@gmail.com>
2022-02-14 14:36:17 +0100

    Removed unused code

--- public/js/voting.js
+++ public/js/voting.js
@@ -7,7 +7,6 @@ module.exports = {
     mounted:function(){
         this.choices()
     },
-    //mounted: this.choices(),
     methods: {
             async choices() {
                 this.votingChoices = await ( await fetch("/gateway/voting/get-choices", {
@@ -18,17 +17,8 @@ module.exports = {
                     var button= document.createElement('button');
                     button.type= 'button';
                     button.appendChild(document.createTextNode(v));
-                    //button.setAttribute('onClick',v.press);
                     document.getElementById("buttons").appendChild(button);
                 } );
-
-
-                /*for(let i = 0; i < this.votingChoices.length; i++) {
-                    let button = document.createElement("button");
-                    button.appendChild(document.createTextNode(v.tooltip));
-                    button.setAttribute('onClick',v.press);
-                    document.getElementById("buttons").appendChild(button);
-                }*/
             }
         }
 
