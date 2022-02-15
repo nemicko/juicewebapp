cdc7ae0f340505a14d80c09af0e31cb562dce6fd
DokiiEU <dorianmarasovic7@gmail.com>
2022-02-15 15:04:48 +0100

    Css Button

--- public/app/landing.vue
+++ public/app/landing.vue
@@ -1,12 +1,12 @@
 <template>
-
   <div class="main">
-    <h1>Landing</h1>
-    <!-- login and setting permissions
-    based on those permissions rerouting to admin.vue or voting.vue -->
-    <button v-on:click="login()">login</button>
-  </div>
+      <h1 class="landing">Landing</h1>
+      <!-- login and setting permissions
+      based on those permissions rerouting to admin.vue or voting.vue -->
+      <button class="logintipka" v-on:click="login()">Login</button>
 
+
+  </div>
 </template>
 
 
