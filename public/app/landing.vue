515b6b44ceecc00a7e2ca4e9794d7b678dfc805a
DokiiEU <dorianmarasovic7@gmail.com>
2022-02-25 13:30:44 +0100

    Landing and Voting modified

--- public/app/landing.vue
+++ public/app/landing.vue
@@ -5,16 +5,14 @@
       <h1 class="text-center headerfont">Voting app</h1>
     </header>
       <div class="text-center container-fluid" id="loginPage">
-        <div class="row">
-          <div class="col-md-12 lowerouterborder">
-            <div class="text-white text-center bg-dark bg-opacity-10 rounded-3 bordercenter">
+          <div class="col-md-12 col-xl-12 col-lg-12 col-sm-12 min-width lowerouterborder">
+            <div class="container py-20 h-15 bordercenter p-15 text-white text-center bg-dark bg-opacity-10 rounded-3">
               <input class="landinginput" type='text' name='login' id='login' placeholder="Enter Password (Admin only)" v-on:keyup.enter="login">
                <!--<button class="logintipka" id="login_button" v-on:click="login()">Admin Login</button>-->
-              <button type="button" class="btn btn-lg MMlogin login-button" id="MMlogin" >MetaMask Login</button>
+              <button type="button" class="btn MMlogin login-button" id="MMlogin" >MetaMask Login</button>
   <!--            <div>{{userWalletAddress}}</div>-->
             </div>
           </div>
-        </div>
       </div>
 <!--    <div id="links" style="display: none">
 
--- public/app/voting.vue
+++ public/app/voting.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="main">
-  <h1 class="header3" id="title">Voting Page</h1>
+    <h1 class="headerfont text-center">Voting Page</h1>
 
   <!-- after login as guest voting choices will be displayed here as buttons -->
 <!--    <div class="adminfont">{{votes}} - Times Clicked</div>-->
-    <div class="container" id="buttons"></div>
-    <div id="votes"></div>
 
-    <!-- router links koji getvotings, svaki ode na voting:id koji onda zove vailable votings i displaya botune-->
-    <div v-for="vote in votings" :key="vote.id">
-      <router-link :to="{ name: 'votingDetails', params: { id: vote.id }}">
-        <span>{{vote.name}}</span>
-      </router-link>
+      <!-- router links koji getvotings, svaki ode na voting:id koji onda zove vailable votings i displaya botune-->
+
+    <div class="container py-20 h-40 p-15 text-white text-center bg-dark bg-opacity-10 rounded-3">
+      <div class="text-center" id="loginPage">
+        <div class="col-md-12 col-xl-12 col-lg-12 col-sm-12 text-center bordercentertitle">
+            <div v-for="vote in votings" :key="vote.id">
+              <router-link style="text-decoration: none;" :to="{ name: 'vote', params: { id: vote.id }}">
+                <span class="text-center font1 hyperlink">{{vote.name}}</span>
+              </router-link>
+            </div>
+            <button class="btn btn-lg login-button" type="button" @click="connect()">Connect Wallet</button>
+            <br>
+            <button class="btn btn-lg login-button" type="button" @click="disconnect()">Disconnect Wallet</button>
+            <br>
+            disconnect
+        </div>
+      </div>
     </div>
-
-
-    <button @click="connect()">Connect Wallet</button>
-
-    <button @click="disconnect()">Disconnect Wallet</button>
-    disconnect
   </div>
 </template>
 
