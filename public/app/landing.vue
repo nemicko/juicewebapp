25f2329e82766152d02da43d7e75fc654af2e664
DokiiEU <dorianmarasovic7@gmail.com>
2022-02-25 14:39:38 +0100

    Voting and Vote Responsive

--- public/app/landing.vue
+++ public/app/landing.vue
@@ -5,11 +5,11 @@
       <h1 class="text-center headerfont">Voting app</h1>
     </header>
       <div class="text-center container-fluid" id="loginPage">
-          <div class="col-md-12 col-xl-12 col-lg-12 col-sm-12 min-width lowerouterborder">
+          <div class="col-md-12 col-xl-12 col-lg-12 col-sm-12">
             <div class="container py-20 h-15 bordercenter p-15 text-white text-center bg-dark bg-opacity-10 rounded-3">
               <input class="landinginput" type='text' name='login' id='login' placeholder="Enter Password (Admin only)" v-on:keyup.enter="login">
                <!--<button class="logintipka" id="login_button" v-on:click="login()">Admin Login</button>-->
-              <button type="button" class="btn MMlogin login-button" id="MMlogin" >MetaMask Login</button>
+              <button type="button" class="col-md-12 col-xl-12 col-lg-12 col-sm-12 btn MMlogin login-button" id="MMlogin" >MetaMask Login</button>
   <!--            <div>{{userWalletAddress}}</div>-->
             </div>
           </div>
--- public/app/vote.vue
+++ public/app/vote.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <h1 class="header3" id="title">Voting Page</h1>
+    <h1 class="headerfont text-center">Voting Page</h1>
 
     <!-- after login as guest voting choices will be displayed here as buttons -->
     <!--    <div class="adminfont">{{votes}} - Times Clicked</div>-->
@@ -8,9 +8,16 @@
     <div id="votes"></div>
 
     <div>
-      <button v-promise-btn v-for="(option, index) in options" @click="vote(index)">
-        {{option}} ({{results[index]}})
-      </button>
+      <div class="container py-20 h-40 p-15 text-white text-center bg-dark bg-opacity-10 rounded-3">
+        <div class="text-center">
+          <div class="col-md-12 col-xl-12 col-lg-12 col-sm-12 text-center bordercentertitle">
+            <button class="text-center login-button" v-promise-btn v-for="(option, index) in options" @click="vote(index)">
+              {{option}} ({{results[index]}})
+            </button>
+            <br>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
