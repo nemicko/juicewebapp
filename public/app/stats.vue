4ec191f33ae1070867214096a557397f6c8857cd
michaelhasler <michael@mhasler.eu>
2022-02-27 21:40:50 +0100

    styling

---
+++ public/app/stats.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="row">
+    <div class="col-1"></div>
+    <div class="col-9">
+      <div v-for="(option, index) in options">
+        {{option}} : {{votings[index]}}
+      </div>
+    </div>
+    <div class="col-1"></div>
+  </div>
+
+</template>
+
+<script src="/js/stats.js"></script>
